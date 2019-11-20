eae46e0de96b74c47f63004624dba407ecfb2152
Teddi Woodson <teddiwood97@ufl.edu>
2019-11-19 19:28:45 -0500

    Add layer two and three variables to main.js

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
 //Global Varibales
 const myBoxes = document.getElementsByClassName('block');
+const myBoxesLayerTwo = document.getElementsByClassName('block-2');
+const myBoxesLayerThree = document.getElementsByClassName('block-3');
 
 //The Functionality
 function loopThrough() {
 
-  const changeColor = () => this.classList.toggle('red');
+  const changeColor = () => {
+    this.classList.toggle('red');
+  };
 
   for (let i = 0; i < this.textContent.length * 2; i++) {
     setTimeout(changeColor, i * 1000);
-  };
-
+  }
 };
 
 //The Event Listener
