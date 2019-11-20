2d50e31300b2b473b66a042eb8672ee83a75172c
Teddi Woodson <teddiwood97@ufl.edu>
2019-11-19 20:18:51 -0500

    Add change layer function from layer 1 to layer 2

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ const myBoxes = document.getElementsByClassName('block');
 //Layer two
 const layerTwo = document.getElementById('layer-two');
 const myBoxesLayerTwo = document.getElementsByClassName('block-2');
+
+//Layer three
+const layerThree = document.getElementById('layer-three');
 const myBoxesLayerThree = document.getElementsByClassName('block-3');
 
 //The Functionality
@@ -35,10 +38,18 @@ function changeLayer() {
   setTimeout(swap, this.textContent.length * 2 * 1000);
 };
 
+function changeLayerTwo() {
+  const swap = () => {
+    layerTwo.classList.add('hide');
+    layerThree.classList.remove('hide');
+  }
+  setTimeout(swap, this.textContent.length * 2 * 1000);
+};
 
 //The Event Listener
 for (let i = 0; i < myBoxes.length; i++) {
   myBoxes[i].addEventListener('click', loopThrough);
   myBoxes[i].addEventListener('click', changeLayer);
   myBoxesLayerTwo[i].addEventListener('click', loopThroughTwo);
+  myBoxesLayerTwo[i].addEventListener('click', changeLayerTwo);
 };
