c3f06533b378700c1ac8e1a7c5e7fc83870c6cf6
Teddi Woodson <teddiwood97@ufl.edu>
2019-11-19 19:15:13 -0500

    Add HW 11 javascript to project 3

---
+++ src/main.js
@@ -0,0 +1,18 @@
+//Global Varibales
+const myBoxes = document.getElementsByClassName('block');
+
+//The Functionality
+function loopThrough() {
+
+  const changeColor = () => this.classList.toggle('red');
+
+  for (let i = 0; i < this.textContent.length * 2; i++) {
+    setTimeout(changeColor, i * 1000);
+  };
+
+};
+
+//The Event Listener
+for (let i = 0; i < myBoxes.length; i++) {
+  myBoxes[i].addEventListener('click', loopThrough);
+};
